b603ac975ebdfa1bb84408d576f98fefbfdaf04a
fedeziraldo <federrico04@gmail.com>
2019-01-19 17:58:14 -0300

    ac usuarios

--- baseDatos.js
+++ baseDatos.js
@@ -5,14 +5,42 @@ db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => {
     console.log("base gymnasio conectada")
 })
-const persona = new mongoose.Schema({
-    nombre: String
+
+const usuario = new mongoose.Schema({
+    nombre: String,
+    password: String,
+    activo: Boolean
 })
 
-const Persona = mongoose.model('Persona', persona)
+const Usuario = mongoose.model('Usuario', usuario)
 
-function listarPersonas() {
-    return Persona.find().exec()
+function findUsuarios() {
+    return Usuario.find()
 }
 
-exports.listarPersonas = listarPersonas
+function findUsuario(usuario) {
+    return Usuario.findOne({ 
+        nombre: new RegExp(usuario.nombre, 'i'),
+        password: usuario.password
+    })
+}
+
+function addUsuario(usuarioDto) {
+    let usuario = new Usuario( {nombre : usuarioDto.nombre, password: usuarioDto.password, activo: false})
+    return usuario.save()
+}
+
+findUsuarios().then( data => {
+    for (elem of data)
+        console.log(elem)
+})
+
+findUsuario({nombre: "fede", password: "cac"}).then( data => {
+    console.log(data)
+})
+
+//console.log(addUsuario({nombre : "fff", password: "bla"}))
+
+exports.addUsuario = addUsuario
+exports.findUsuario = findUsuario
+exports.findUsuarios = findUsuarios
